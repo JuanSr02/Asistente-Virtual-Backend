4db9c62880fdf51f35160e83331ddb45ff534212
JuanSr02 <juanma2002123@gmail.com>
2025-06-13 09:25:54 -0300

    update

--- src/main/java/com/recommendationSys/Sistema_Recomendador_Finales/SistemaRecomendadorDeFinalesApplication.java
+++ src/main/java/com/recommendationSys/Sistema_Recomendador_Finales/SistemaRecomendadorDeFinalesApplication.java
@@ -14,7 +14,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories(basePackages = "com.recommendationSys.Sistema_Recomendador_Finales.repository")
 public class SistemaRecomendadorDeFinalesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SistemaRecomendadorDeFinalesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaRecomendadorDeFinalesApplication.class, args);
+    }
 }
