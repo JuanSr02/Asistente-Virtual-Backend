bc184f045b4e0f3d7ded8cf54cd967a3b84ebc14
JuanSr02 <juanma2002123@gmail.com>
2025-06-17 10:56:12 -0300

    Changes.

--- src/main/java/TestConnection.java
+++ src/main/java/TestConnection.java
@@ -3,9 +3,9 @@ import java.sql.DriverManager;
 
 public class TestConnection {
     public static void main(String[] args) {
-        String url = "jdbc:postgresql://localhost:5432/asistente-virtual";
-        String user = "Juan";
-        String password = "admin";
+        String url = "jdbc:postgresql://aws-0-sa-east-1.pooler.supabase.com:5432/postgres";
+        String user = "postgres.qlbpcnyjsvhxnncjorku";
+        String password = "unsl2025";
 
         try (Connection conn = DriverManager.getConnection(url, user, password)) {
             System.out.println("✅ Conexión exitosa!");
