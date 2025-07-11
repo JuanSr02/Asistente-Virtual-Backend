136aaf14c286670344013732ef3bea88f40bb812
JuanSr02 <juanma2002123@gmail.com>
2025-07-11 19:37:20 -0300

    Ping Pong Controller.

---
+++ src/main/java/com/asistenteVirtual/controllers/PingPongController.java
@@ -0,0 +1,16 @@
+package com.asistenteVirtual.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/public/ping")
+public class PingPongController {
+
+    @GetMapping
+    public ResponseEntity<String> pingPong() {
+        return ResponseEntity.ok("pong");
+    }
+}
